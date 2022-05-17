7978a0b278211078e8649e082c30467f25046379
ntnfolla <ntnfolla@gmail.com>
2022-05-17 06:35:27 -0700

    main file

--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
 
 int _putchar(char c);
 int print_int(va_list arg);
